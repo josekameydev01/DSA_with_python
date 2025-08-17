1219a213b2b76880d5bc72589e9c99479fcb645d
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-16 20:44:06 -0600

    How to read all elements from a static array

---
+++ static_arrays/how_to_read_all_elements.py
@@ -0,0 +1,15 @@
+# Initialize my array.
+# Time complexity: O(1)
+my_array = [1,3,5]
+
+# Reading all values using a for loop.
+# Time complexity: O(N)
+for i in range(len(my_array)):
+    print(my_array[i])
+
+# Reading all values using a while loop.
+# Time complexity: O(N)
+i = 0
+while i < len(my_array):
+    print(my_array[i])
+    i += 1
