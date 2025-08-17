52f39080d8d420fefdffece8f3c6a05af6ad8f4c
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-16 20:16:19 -0600

    How to read an individual element from a static array

---
+++ static_arrays/how_to_read_an_element.py
@@ -0,0 +1,9 @@
+# Initialize an array
+# Time complexity: O(1)
+my_array = [1, 3, 5]
+
+# Reading an arbitray element from a array.
+# Time complexity: O(1)
+print(my_array[0])
+print(my_array[1])
+print(my_array[2])
