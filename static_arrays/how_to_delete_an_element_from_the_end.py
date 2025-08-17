51413a83f5567254bbf2ab0ae517d5a7533c96e7
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-16 20:55:53 -0600

    How to delete an element from the end of a static array

--- static_arrays/how_to_delete_an_element_from_the_end.py
+++ static_arrays/how_to_delete_an_element_from_the_end.py
@@ -1,3 +1,5 @@
+# Function to remove the last element.
+# Time complexity: O(1)
 def removeEnd(arr, length):
     if length > 0:
         arr[length - 1] = None
@@ -7,6 +9,8 @@ def removeEnd(arr, length):
 my_array = [1,3,5]
 length = len(my_array)
 
+# Deleting all elements of an array.
+# Time complexity: O(N)
 for _ in range(len(my_array) + 1):
     print(my_array)
     removeEnd(my_array, length)
